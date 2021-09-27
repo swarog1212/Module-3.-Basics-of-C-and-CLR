934a461f46d6a91f35b6bea47a768f7cf0774708
Георгий Попов <dark8609@outlook.com>
2021-09-27 11:58:57 +0300

    Ура, я инициализировал переменную

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "George";
+            Console.WriteLine(myName);
+            Console.ReadKey();
+        }
+    }
+}
