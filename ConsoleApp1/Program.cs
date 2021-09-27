46939323294ec61ca1bd27f0fa6a8e7c3023c614
Георгий Попов <dark8609@outlook.com>
2021-09-27 12:03:48 +0300

    Инициализируем переменную при объявлении

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,8 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string myName;
-            myName = "George";
+            string myName = "George";
+
             Console.WriteLine(myName);
             Console.ReadKey();
         }
